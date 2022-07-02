70fe61a023a254985b660d57e64f091a6daca033
kyawswarhtun <server1.kst@gmail.com>
2022-07-02 13:25:38 +0630

    add sample C project

---
+++ build/CMakeFiles/thetthet.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/thetthet.dir/main.cpp.o"
+  "thetthet"
+  "thetthet.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/thetthet.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
